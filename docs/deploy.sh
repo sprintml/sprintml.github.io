fa5da9b7639706324b8890cdc1aa3840acb8d684
fraboeni <franziska.boenisch.fb@gmail.com>
2023-05-27 19:42:51 -0400

    deploy at timestamp: 2023-05-27-19:42:23

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -2,15 +2,25 @@ timestamp=$(date "+%Y-%m-%d-%H:%M:%S")
 jekyll build --destination docs
 
 # for this private sprintml-lab repo
+git pull
 git add *
 git commit -am "deploy at timestamp: ${timestamp}"
 git push origin master
 
+# save the absolute path of the current working directory to the variable cwd
+cwd=$(pwd)
+
+# go to the webpage public repo and push the changes
+cd ../sprintml.github.io
+git pull
+
 # move the docs to the webpage public repo
-cp -r docs ../sprintml.github.io
+cp -r ../sprintml-lab/docs .
 
 # go to the webpage public repo and push the changes
 cd ../sprintml.github.io
 git add *
 git commit -am "deploy at timestamp: ${timestamp}"
 git push origin main
+
+cd cwd
